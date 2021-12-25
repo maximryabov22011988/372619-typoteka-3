bab6f781652d0261dbd4a5e5a6a69576e7f7e3b4
Ryabov Maxim <ryabovms@altarix.ru>
2021-12-25 10:53:03 +0400

    module2-task2

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -16,56 +16,9 @@ const DEFAULT_COUNT = 1;
 const MAX_COUNT = 1000;
 const MAX_COUNT_ERROR_MESSAGE = `Не больше ${MAX_COUNT} публикаций`;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`
-];
-
-const DESCRIPTIONS = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`
-];
-
+const TITLES_FILE_PATH = `./data/titles.txt`;
+const SENTENCES_FILE_PATH = `./data/sentences.txt`;
+const CATEGORIES_FILE_PATH = `./data/categories.txt`;
 
 const formatDate = (date) => {
   if (date instanceof Date) {
@@ -93,15 +46,25 @@ const createDate = () => {
   return new Date(randomDateInMs);
 };
 
-const generatePublications = (count) => (
+const generatePublications = ({count, titles, sentences, categories}) => (
   Array.from({length: count}, () => ({
-    title: getRandomArrElement(TITLES),
-    announce: getRandomArrElements(DESCRIPTIONS, 5).join(` `),
-    fullText: getRandomArrElements(DESCRIPTIONS).join(` `),
+    title: getRandomArrElement(titles),
+    announce: getRandomArrElements(sentences, 5).join(` `),
+    fullText: getRandomArrElements(sentences).join(` `),
     createdDate: formatDate(createDate()),
-    category: getRandomArrElements(CATEGORIES),
+    category: getRandomArrElements(categories),
   }))
 );
+
+const readContent = async (filepath) => {
+  try {
+    const content = await fs.readFile(filepath, `utf-8`);
+    return content.trim().split(`\n`);
+  } catch (e) {
+    console.error(chalk.red(e));
+    return [];
+  }
+};
 
 module.exports = {
   name: `--generate`,
@@ -114,7 +77,16 @@ module.exports = {
       process.exit(ExitCode.ERROR);
     }
 
-    const content = JSON.stringify(generatePublications(publicationsCount), null, 2);
+    const titles = await readContent(TITLES_FILE_PATH);
+    const sentences = await readContent(SENTENCES_FILE_PATH);
+    const categories = await readContent(CATEGORIES_FILE_PATH);
+    const publications = generatePublications({
+      count: publicationsCount,
+      titles,
+      sentences,
+      categories
+    });
+    const content = JSON.stringify(publications, null, 2);
 
     try {
       await fs.writeFile(MOCK_FILENAME, content);
