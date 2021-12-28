5a4d2113c7274decc610463de923f3a8e864e16b
Ryabov Maxim <ryabovms@altarix.ru>
2021-12-28 16:19:13 +0400

    module3-task1 refactor

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,14 +1,8 @@
 'use strict';
 
-const help = require(`./help`);
-const version = require(`./version`);
-const generate = require(`./generate`);
-const server = require(`./server`);
-
-const Cli = [help, version, generate, server].reduce((result, module) => {
-  result[module.name] = module;
-  return result;
-}, {});
+const {
+  Cli
+} = require(`./modules`);
 
 module.exports = {
   Cli
---
+++ src/service/cli/modules.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const help = require(`./help`);
+const version = require(`./version`);
+const generate = require(`./generate`);
+const server = require(`./server`);
+
+const Cli = [help, version, generate, server].reduce((result, module) => {
+  result[module.name] = module;
+  return result;
+}, {});
+
+module.exports = {
+  Cli
+};
