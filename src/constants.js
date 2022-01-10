5efe88db3971452e88645ab3fdff5a8965dba12a
Ryabov Maxim <ryabovms@altarix.ru>
2022-01-10 09:24:02 +0400

    module3-task1

--- src/constants.js
+++ src/constants.js
@@ -14,10 +14,11 @@ const ExitCode = {
 
 const HttpCode = {
   OK: 200,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
   NOT_FOUND: 404,
   INTERNAL_SERVER_ERROR: 500,
-  FORBIDDEN: 403,
-  UNAUTHORIZED: 401,
 };
 
 
---
+++ src/express/express.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require(`express`);
+const {HttpCode} = require(`../constants`);
+const mainRoutes = require(`./routes/main`);
+const myRoutes = require(`./routes/my`);
+const articlesRoutes = require(`./routes/articles`);
+
+const DEFAULT_PORT = 8080;
+
+const app = express();
+
+app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
+app.use(`/`, mainRoutes);
+app.use((req, res) => res.status(HttpCode.BAD_REQUEST).send(`Not found`));
+app.use((err, req, res) => res.status(HttpCode.INTERNAL_SERVER_ERROR).send(`Internal server error`));
+
+app.listen(DEFAULT_PORT);
