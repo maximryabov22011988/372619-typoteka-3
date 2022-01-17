1b1de7d9dce10319b180f0f4723696bfab529363
Ryabov Maxim <ryabovms@altarix.ru>
2022-01-17 21:15:15 +0400

    module3-task3

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,58 +1,17 @@
 'use strict';
 
-const fs = require(`fs`).promises;
-const http = require(`http`);
+const express = require(`express`);
 const chalk = require(`chalk`);
-
-const {
-  MOCK_FILENAME,
-  HttpCode
-} = require(`../../constants`);
+const {HttpCode} = require(`../../constants`);
+const postsRoutes = require(`../routes/posts`);
 
 const DEFAULT_PORT = 3000;
+const notFoundMessageText = `Not found`;
 
-const sendResponse = (res, statusCode, message) => {
-  const template = `
-    <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <title>Typoteka</title>
-      </head>
-      <body>
-        ${message}
-      </body>
-    </html>
-  `;
-
-  res.writeHead(statusCode, {
-    'Content-Type': `text/html; charset=UTF-8`,
-  });
-  res.end(template);
-};
-
-
-const onClientConnect = async (req, res) => {
-  const notFoundMessageText = `Not found`;
-
-  switch (req.url) {
-    case `/`: {
-      try {
-        const content = await fs.readFile(MOCK_FILENAME, `utf-8`);
-        const publications = JSON.parse(content);
-        const titles = publications.map(({title}) => `<li>${title}</li>`).join(``);
-        sendResponse(res, HttpCode.OK, `<ul>${titles}</ul>`);
-      } catch (e) {
-        sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-      }
-      break;
-    }
-    default: {
-      sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-    }
-  }
-};
-
+const app = express();
+app.use(express.json());
+app.use(`/posts`, postsRoutes);
+app.use((req, res) => res.status(HttpCode.BAD_REQUEST).send(notFoundMessageText));
 
 module.exports = {
   name: `--server`,
@@ -60,13 +19,12 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, () => {
-        console.info(chalk.green(`Waiting for connections on port ${port}`));
-      })
-      .on(`error`, ({message}) => {
-        console.error(chalk.red(`Server creation error: ${message}`));
-      });
+    app.listen(port, (err) => {
+      if (err) {
+        return console.error(chalk.red(`Server creation error: ${err.message}`));
+      }
+
+      return console.info(chalk.green(`Waiting for connections on port ${port}`));
+    });
   }
 };
