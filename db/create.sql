95f3bec0e17efd6852514b8010bace88f85085f5
Ryabov Maxim <ryabovms@altarix.ru>
2022-03-09 22:15:26 +0400

    module5-task1

---
+++ db/create.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE typoteka
+  WITH
+  OWNER = academy
+  ENCODING = 'UTF8'
+  TEMPLATE template0
+  LC_COLLATE = 'C'
+  LC_CTYPE = 'C'
+  CONNECTION LIMIT = - 1;
+
+GRANT ALL ON DATABASE typoteka TO academy;
