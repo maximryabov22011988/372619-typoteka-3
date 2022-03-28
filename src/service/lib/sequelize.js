12cd000de69a9a8c41fcc50954a15bbb5f9231a9
Ryabov Maxim <ryabovms@altarix.ru>
2022-03-28 21:03:26 +0400

    module6-task2 fix

--- src/service/lib/sequelize.js
+++ src/service/lib/sequelize.js
@@ -1,24 +1,31 @@
 'use strict';
 
 const Sequelize = require(`sequelize`);
+const {USER_ARGV_INDEX} = require(`../../constants`);
+
 const {DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD} = process.env;
+const userArgs = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArgs;
 
-const hasAllRequiredDbEnvVariables = [DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD].some((variable) => variable !== undefined);
+if (userCommand.includes(`server`)) {
+  const hasAllRequiredDbEnvVariables = [DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD].some((variable) => variable !== undefined);
 
-if (!hasAllRequiredDbEnvVariables) {
-  throw new Error(`One or more database environmental variables are not defined`);
+  if (!hasAllRequiredDbEnvVariables) {
+    throw new Error(`One or more database environmental variables are not defined`);
+  }
+
+  const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: `postgres`,
+    pool: {
+      min: 3,
+      max: 7,
+      acquire: 10000,
+      idle: 10000
+    }
+  });
+
+  module.exports = sequelize;
 }
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: `postgres`,
-  pool: {
-    min: 0,
-    max: 10,
-    acquire: 10000,
-    idle: 10000
-  }
-});
-
-module.exports = sequelize;
